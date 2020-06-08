693737aede737c49eaef003bd285df2dfbe0a662
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-08 17:51:31 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,5 +2,13 @@ class Person
   def name=(this_name)
     @name = this_name 
   end 
-  def 
+  def name 
+    @name 
+  end 
+  def job=(this_job)
+    @job = this_job
+  end 
+  def job
+    @job
+    end 
   end 
