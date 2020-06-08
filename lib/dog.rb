8be5bdfe04163c7ba01f50be027759e87d272b48
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-08 17:48:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,17 @@
-class Dog
-  def initialize(name)
-    @name = name
+ class Dog
+  def name=(dog_name)
+    @name = dog_name
   end
- 
+
   def name
     @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
   end
 end
